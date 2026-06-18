b989d590042618b3605245d3d30e1f5d24391eba
Joel Sanz Martí <joel04slm@gmail.com>
2026-06-18 19:00:04 +0200

    set

--- index.js
+++ index.js
@@ -30,6 +30,34 @@ app.get('/estado', (req, res) => {
     res.json(fabrica);
 });
 
-app.post('/estado', (req, res) => {
-    fabrica = req.body;
+app.post('/nivelEnergia', (req, res) => {
+    fabrica.nivelEnergia = req.body;
+});
+
+app.post('/consumo', (req, res) => {
+    fabrica.generador.consumo = req.body;
+});
+
+app.post('/estadoGenerador', (req, res) => {
+    fabrica.generador.estado = req.body;
+});
+
+app.post('/alarma', (req, res) => {
+    fabrica.alarma = req.body;
+});
+
+app.post('/ciclos', (req, res) => {
+    fabrica.prensa.ciclos = req.body;
+});
+
+app.post('/estadoPrensa', (req, res) => {
+    fabrica.prensa.estado = req.body;
+});
+
+app.post('/estadoPuerta', (req, res) => {
+    fabrica.puerta.estado = req.body;
+});
+
+app.post('/ultimoAcceso', (req, res) => {
+    fabrica.puerta.ultimoAcceso = req.body;
 });
