3bd15d17c0d0cffea1a062b7f8d71c2b6377aae8
Joel Sanz Martí <joel04slm@gmail.com>
2026-06-18 19:40:56 +0200

    si

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-
+ 
 app.use(express.json())
-
+ 
 let fabrica = {
     modoFabrica: "normal",
     alarma: false,
@@ -20,44 +20,52 @@ let fabrica = {
         consumo: 0
     }
 };
-
+ 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Servidor funcionando");
 });
-
+ 
 app.get('/estado', (req, res) => {
     res.json(fabrica);
 });
-
+ 
 app.post('/nivelEnergia', (req, res) => {
     fabrica.nivelEnergia = req.body;
+    res.sendStatus(200);
 });
-
+ 
 app.post('/consumo', (req, res) => {
     fabrica.generador.consumo = req.body;
+    res.sendStatus(200);
 });
-
+ 
 app.post('/estadoGenerador', (req, res) => {
     fabrica.generador.estado = req.body;
+    res.sendStatus(200);
 });
-
+ 
 app.post('/alarma', (req, res) => {
     fabrica.alarma = req.body;
+    res.sendStatus(200);
 });
-
+ 
 app.post('/ciclos', (req, res) => {
     fabrica.prensa.ciclos = req.body;
+    res.sendStatus(200);
 });
-
+ 
 app.post('/estadoPrensa', (req, res) => {
     fabrica.prensa.estado = req.body;
+    res.sendStatus(200);
 });
-
+ 
 app.post('/estadoPuerta', (req, res) => {
     fabrica.puerta.estado = req.body;
+    res.sendStatus(200);
 });
-
+ 
 app.post('/ultimoAcceso', (req, res) => {
     fabrica.puerta.ultimoAcceso = req.body;
+    res.sendStatus(200);
 });
